f106d27dc9e525967507373f5405faa14c944673
Efim K <Efim K@DESKTOP-QA8A53H>
2022-11-09 16:26:11 +0300

    Добавьте файлы проекта.

---
+++ DZ_9_1/Program.cs
@@ -0,0 +1,23 @@
+﻿class CustomArray<T>
+{
+    public CustomArray(int count)
+        {
+        T[] array = new T[count];
+        }
+    public void Sort(T[] array)
+    {
+        T temp;
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] > array[j])
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
